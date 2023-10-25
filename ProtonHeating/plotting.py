c34cc8e767550ab112d4ca4930c4a27291a94249
Rowan Dayton-Oxland <r.daytonoxland@outlook.com>
2023-10-25 15:06:59 +0100

    first plotting.py

--- ProtonHeating/plotting.py
+++ ProtonHeating/plotting.py
@@ -1,12 +1,19 @@
 from matplotlib import pyplot as pyplot
 import numpy as np 
 
-def readall(fstart, data)
 
-types = [intensity, blueshift, temperature]
+def readall(fstart, data):
+"""
 
-fnames = []
-[fnames.append('~/lib/' + fstart + types[i]) for i in len(2)]
+"""
 
-data = np.array[3]
-[data[i] = open(fnames[i], 'r') for i in len(2)]
+    types = [intensity, blueshift, temperature]
+
+    fnames = []
+    [fnames.append('~/lib/' + fstart + types[i]) for i in len(2)]
+
+    data = np.array[3]
+    [data[i] = open(fnames[i], 'r') for i in len(2)]
+
+    return data
+
